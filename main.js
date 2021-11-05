d1023d27dfeb1edda1bef67ed833781cbf7dde92
mrspyyt <piotrus.dudek@op.pl>
2021-11-05 18:22:33 +0100

    update <commands>
    adding new commands

--- main.js
+++ main.js
@@ -7,16 +7,73 @@ const command = require('./command')
 
 client.on('ready', () => {
   console.log('The client is ready!')
+  client.channels.cache.get(`905950843944177714`).send('Spy is working!');
+
+
+  command(client, 'sw' , (message) => {
+    const name = message.content.replace('!sw ', '')
+    client.channels.cache.get(`905950843944177714`).send('Spy is stoped working!');
+    })
+
+
+    
+
+
+
+
+    command(client, 't' , (message) => {
+        const name = message.content.replace('!wip ', '')
+        message.delete()
+        message.channel.send('Work in progres !')
+    })
+
+    command(client, '25' , (message) => {
+        const name = message.content.replace('!25 ', '')
+        message.delete()
+        message.channel.send('Project is around 25% !')
+    })
+
+    command(client, '50' , (message) => {
+        const name = message.content.replace('!50 ', '')
+        message.delete()
+        message.channel.send('Project is around 50% !')
+    })
+
+    command(client, '75' , (message) => {
+        const name = message.content.replace('!75 ', '')
+        message.delete()
+        message.channel.send('Project is around 75% !')
+    })
+
+    command(client, '100' , (message) => {
+        const name = message.content.replace('!100 ', '')
+        message.delete()
+        message.channel.send('your project is almost complite !')
+    })
+
+    command(client, 'pay' , (message) => {
+        const name = message.content.replace('!pay ', '')
+        message.delete()
+        message.channel.send('your project requaiers payment to continue working on project!')
+    })
+
+    command(client, 'file' , (message) => {
+        const name = message.content.replace('!sfile ', '')
+        message.delete()
+        message.channel.send('we ll send you yors project file !')
+    })
+
 
   command(client, 'createtextchannel', (message) => {
     const name = message.content.replace('!createtextchannel ', '')
+    
 
     message.guild.channels
       .create(name, {
         type: 'text',
       })
       .then((channel) => {
-        const categoryId = '842196489396420638'
+        const categoryId = '905955727108096010'
         channel.setParent(categoryId)
       })
   })
@@ -29,11 +86,11 @@ client.on('ready', () => {
         type: 'voice',
       })
       .then((channel) => {
-        const categoryId = '842196489396420638'
+        const categoryId = '905955727108096010'
         channel.setParent(categoryId)
         channel.setUserLimit(10)
       })
   })
 })
 
-client.login('OTAxMTU2NTMwNTA4OTQzNDMx.YXLxaw.5VdNc1TfL9i8sckNvifiXOhLixo')
+client.login('token');
